22893e693de72c2fdff31e8da64f92e1621a38e0
sankarkarnati <118084702+sankarkarnati@users.noreply.github.com>
2022-11-24 17:14:14 +0000

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,3 +11,8 @@ def newDeploy(jobname,ip,appname)
 {
 sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
+
+def newSelenium(jobname)
+{
+sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
+}
